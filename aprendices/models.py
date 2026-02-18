66f911255281b69af9eb9fccb4926e4751768e95
Daniel7 <dl4041569@gmail.com>
2026-02-18 18:49:42 -0500

    feat(db): cambiar campos ficha y programa a ForeignKey en modelo Aprendiz

--- aprendices/models.py
+++ aprendices/models.py
@@ -5,9 +5,12 @@ class Aprendiz(models.Model):
     # Relación 1 a 1: Un usuario del sistema es UN aprendiz
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     
+    # --- AQUÍ ESTÁ LA MAGIA DE LAS LLAVES FORÁNEAS ---
+    # En lugar de CharField, usamos ForeignKey apuntando a las otras apps
+    numero_ficha = models.ForeignKey('fichas.Ficha', on_delete=models.RESTRICT, verbose_name="Número de Ficha")
+    programa_formacion = models.ForeignKey('programas.Programa', on_delete=models.RESTRICT, verbose_name="Programa de Formación")
+    
     # Datos exclusivos del SENA
-    numero_ficha = models.CharField(max_length=20, verbose_name="Número de Ficha")
-    programa_formacion = models.CharField(max_length=150, verbose_name="Programa de Formación")
     telefono = models.CharField(max_length=20, verbose_name="Celular")
     documento = models.CharField(max_length=20, null=True, blank=True, verbose_name="Documento de Identidad")
     
